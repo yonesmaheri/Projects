02072bef19655d69ee23c00bbc700263dcb072c4
yonesmaheri <yonesmaheri80@gmail.com>
2025-12-19 11:02:50 +0330

    add loading more

--- HW-L03-02 React/gallery/src/App.jsx
+++ HW-L03-02 React/gallery/src/App.jsx
@@ -2,34 +2,47 @@ import { useEffect, useState } from "react";
 import PhotoList from "./components/PhotoList";
 
 function App() {
+
+
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
 
   useEffect(() => {
-    const fetchPhotos = async () => {
-      try {
-        const res = await fetch(
-          "https://api.pexels.com/v1/curated?per_page=12",
-          {
-            headers: {
-              Authorization: 'uQEI62kXZVgQTz1VvrW1CkSvd72DfPVhhGGOD4JIjw7hwToTa1WytcC8',
-            },
-          }
-        );
+  setLoading(true);
 
-        const data = await res.json();
-        setPhotos(data.photos);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  fetch(
+    `https://api.pexels.com/v1/curated?per_page=9&page=${page}`,
+    {
+      headers: {
+        Authorization: 'uQEI62kXZVgQTz1VvrW1CkSvd72DfPVhhGGOD4JIjw7hwToTa1WytcC8',
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      setPhotos((prev) => [...prev, ...data.photos]);
+    })
+    .finally(() => setLoading(false));
+}, [page]);
 
-    fetchPhotos();
-  }, []);
 
   return (
     <div className="bg-slate-100 min-h-screen py-10">
       <div className="max-w-5xl mx-auto">
-        <PhotoList photos={photos} />
+        <PhotoList photos={photos || []} />
+
+        <div className="flex justify-center mt-8">
+          <button
+            onClick={() => setPage((prev) => prev + 1)}
+            disabled={loading}
+            className="px-5 py-2 rounded-lg bg-blue-100 text-blue-600
+            font-medium hover:bg-blue-200 transition disabled:opacity-50"
+          >
+            {loading ? "Loading..." : "Load More"}
+          </button>
+        </div>
       </div>
     </div>
   );
